4ff9173ec3c4810536a40f859e71b5f3e4fa1434
xurde <jorgedf@gmail.com>
2012-05-29 00:48:19 -0400

    User Unit tests working

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,20 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :name, :remember_me
+  
+  # Validations
+  validates_presence_of :email, :name
+  validates_uniqueness_of :email, :on => :create, :message => "is already taken", :case_sensitive => false
+  validates_presence_of :email, :if => :password_present?
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password_present?
+  validates_length_of :password, :within => 5..15, :message => "must be between 5 and 15 chars long", :if => :password_present?
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  
+  
+  private
+  
+  def password_present?
+    !password.nil?
+  end
+  
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    
+  test "user creation" do
+    user = User.new(:email => 'usuario@dominio.com', :name => 'Nombre de usuario', :password => 'password01', :password_confirmation => 'password01')
+    assert user.save
+    if !user.new_record?
+      assert user.destroy
+    else
+      skip "User not saved"
+    end
+  end
+  
+  test "wrong password user creation" do
+    user = User.new(:email => 'usuario@dominio.com', :name => 'Nombre de usuario', :password => 'password01', :password_confirmation => 'password')
+    assert !user.save
+  end
+
+  test "wrong email user creation" do
+    user = User.new(:email => 'qwerty@qwerty', :name => 'Nombre de usuario', :password => 'password01', :password_confirmation => 'password01')
+    assert !user.save
+  end
+  
 end
