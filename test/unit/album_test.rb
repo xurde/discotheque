e3c8e5e5138e6d16f7fd2581f0726a32b74c9034
xurde <jorgedf@gmail.com>
2012-06-06 03:55:18 -0400

    Unit tests for Album model added.

--- test/unit/album_test.rb
+++ test/unit/album_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  user = User.new(:email => 'usuario@dominio.com', :name => 'Nombre de usuario', :password => 'password01', :password_confirmation => 'password01')
+
+  test "Album creation" do
+    album = Album.new(:user => user, :band => 'The Beatles', :title => 'Revolver', :year => 1965)
+    assert album.save
+    if !album.new_record?
+      assert album.destroy
+    else
+      skip "Album not saved!"
+    end
+  end
+  
+  test "wrong Band on album creation" do
+    album = Album.new(:user => user, :band => '-', :title => 'Revolver', :year => 1965)
+    assert !album.save
+  end
+
 end
